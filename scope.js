ea9b67213152d8db0267dbabed88f1276406cb99
ashiqdotexe <ashiqurrahman.sohan.muslim@gmail.com>
2025-12-25 14:06:10 +0600

    Scope Basic-2

--- scope.js
+++ scope.js
@@ -2,6 +2,11 @@ var lang = "Bangla";
 
 function learn(topic){
     var lang = topic;
+    if(true){
+        let a =5;
+        a=6;
+        console.log(`the number is ${a}`)
+    }
     console.log(`I am learning ${lang}`)
 }
 learn("Javascript")
